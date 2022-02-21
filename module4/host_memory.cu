45c02c661fa7a4ef1ac3b1e6ff38c51e46801e35
Yujiang Wu <wuyujiangacademy@yahoo.com>
2022-02-20 21:47:26 -0600

    Added some comments

--- module4/host_memory.cu
+++ module4/host_memory.cu
@@ -5,32 +5,33 @@
 __global__
 void saxpy(int n, float a, float *x, float *y)
 {
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  int i = blockIdx.x*blockDim.x + threadIdx.x;//global index calculation
   if (i < n) y[i] = a*x[i] + y[i];
 }
 
 int main(void)
 {
-  int N = 1<<20;
+  int N = 1<<8;
   float *x, *y, *d_x, *d_y;
+  // (pagable) host mem allocation
   x = (float*)malloc(N*sizeof(float));
   y = (float*)malloc(N*sizeof(float));
-
+// device memory allocation
   cudaMalloc(&d_x, N*sizeof(float)); 
   cudaMalloc(&d_y, N*sizeof(float));
-
+// host memory initialization
   for (int i = 0; i < N; i++) {
     x[i] = 1.0f;
     y[i] = 2.0f;
   }
-
-  cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+// explicity memory copy: 
+  cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);// x-->d_x
+  cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);//y-->d_y
 
   // Perform SAXPY on 1M elements
   saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
-
-  cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+// explicity memory copy: 
+  cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);//d_y--->y
 
   float maxError = 0.0f;
   for (int i = 0; i < N; i++){
