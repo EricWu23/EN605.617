795d579f922e588c856780cb09d12847909c2929
Yujiang Wu <wuyujiangacademy@yahoo.com>
2022-04-17 21:52:58 -0500

    intermediate commit
    move the core functionality of Convolution.cpp into the testharness.cpp and make the main function of  assignment.cpp very short

---
+++ module11/assignment.cpp
@@ -0,0 +1,27 @@
+//
+// Book:      OpenCL(R) Programming Guide
+// Authors:   Aaftab Munshi, Benedict Gaster, Timothy Mattson, James Fung, Dan Ginsburg
+// ISBN-10:   0-321-74964-2
+// ISBN-13:   978-0-321-74964-2
+// Publisher: Addison-Wesley Professional
+// URLs:      http://safari.informit.com/9780132488006/
+//            http://www.openclprogrammingguide.com
+//
+
+// Convolution.cpp
+//
+//    This is a simple example that demonstrates basic OpenCL setup and
+//    use.
+
+#include "assignment.h"
+#include "testharness.h"
+
+
+///
+//	main() for Convolution example
+//
+int main(int argc, char** argv)
+{
+   testKernel("convolve");
+   return 0;
+}
---
+++ module11/testharness.h
@@ -0,0 +1,2 @@
+#pragma once
+int testKernel(const char *  kernel_name);
